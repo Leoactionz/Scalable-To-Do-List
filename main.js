2c7c72e2ab80d3cc89ddd4a450d80ce4f14d6359
LeotheUXBro <leoucu@gmail.com>
2021-06-27 18:02:27 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,77 @@
+// UI CONTROLLER
+let UIController = (() => {
+    // Domstrings make programmers lives easier enables quick update here instead of updating each methods when there is a change on the interface
+    let DOMstrings = {
+        addToDo: ".addToDo",
+        inputField: ".inputField",
+        toDoContainer: ".toDoContainer",
+    };
+
+    return {
+        getInput: () => {
+            return {
+                addBtn: document.querySelector(DOMstrings.addToDo),
+                todo: document.querySelector(DOMstrings.inputField),
+                container: document.querySelector(DOMstrings.toDoContainer),
+            };
+        },
+
+        // Exposing the DOMstrings to make it public
+        getDOMStrings: () => {
+            return DOMstrings;
+        }
+
+    };
+})();
+
+
+let controller = ((UICtrl) => {
+
+    let setupEventListeners = function () {
+        let DOM = UICtrl.getDOMStrings();
+
+        document.querySelector(DOM.addToDo).addEventListener("click", ctrlAddItem);
+
+        // keypress listener happened mostly on the global scope (Enter Key input)
+        document.addEventListener("keypress", (event) => {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
+    let ctrlAddItem = () => {
+        let input, paragraph;
+
+        // 1. Get the filled input
+        input = UICtrl.getInput();
+        // 2. Create element to store input on HTML
+        paragraph = document.createElement("p");
+        // 3. Take in the inputted value
+        paragraph.innerText = input.todo.value;
+        // 4. Add create element to an existing element in html
+        input.container.appendChild(paragraph);
+        // 5. Clear the fields
+        input.todo.value = '';
+        
+        // 6. Done entry
+        paragraph.addEventListener('click', () => {
+            paragraph.style.textDecoration = "line-through";
+        })
+        
+        // 7. Delete entry
+        paragraph.addEventListener('dblclick', function () {
+            input.container.removeChild(paragraph);
+        })
+
+    };
+
+    return {
+        init: () => {
+            console.log("Application has started");
+            setupEventListeners();
+        },
+    };
+})(UIController);
+
+controller.init();
